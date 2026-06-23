a135017ef0d4c7bcf99b5da4dbb4323c48fbbddc
xzming <xzming@163.com>
2026-06-23 08:28:35 +0800

    Fix: 添加根路由index.tsx，启动时先进封面页

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,6 +10,7 @@ export default function RootLayout() {
         <Stack
           screenOptions={{ headerShown: false }}
         >
+          <Stack.Screen name="index" />
           <Stack.Screen name="welcome" />
           <Stack.Screen name="(tabs)" />
           <Stack.Screen name="job/[id]" />
